f9d0ecf5b931ae9094b5b2c2f4526daa430cce9f
SarahiJLeura <plsj.los3005aiza@gmail.com>
2025-11-02 18:50:24 -0600

    Upload file to read images and recognize objects in a video

--- object_detection.py
+++ object_detection.py
@@ -1,34 +1,89 @@
 from descriptors.orb import ORB
-import numpy as np
+from descriptors.sift import SIFT
+from descriptors.hog_svm import HOG_SVM
+import pandas as pd
 import os
 import cv2
 
-# Append all the target images
-img_path = "VisionComputacional/Examenes/Ex02/resources/test" # Save the root path
+
+def read_images(src, path_images):
+    images = []
+    for path in path_images:
+        imgCur = cv2.imread(f'{src}/{path}')
+        if imgCur is None:
+            raise ValueError(f"Cannot load image from {src}/{path}")
+        images.append(imgCur)
+    return images
+
+# ---------------------- IMAGES FOR ORB AND SIFT ------------------------
+img_path = "VisionComputacional/Examenes/Ex02/resources/train" # Save the root path
 images = []
-classNames = []
-testImages = os.listdir(img_path) # List target images of the directory
-print("Total class detected: ", len(testImages))
+classNames = ["calendar", "notebook", "raid"]
+trainImages = os.listdir(img_path)
+trainImages.pop(1)
 
-# Loop to read and append the images
-for cl in testImages:
-    imgCur = cv2.imread(f'{img_path}/{cl}')
-    if imgCur is None:
-        raise ValueError(f"Cannot load image from {img_path}/{cl}")
-    images.append(imgCur)
-    classNames.append(os.path.splitext(cl)[0]) # Stores the classification names
+print(trainImages)
+print("Total class detected: ", len(trainImages))
 
-print(classNames) # print classification names
+images = read_images(img_path, trainImages)
 
-# Append all the train videos
-vid_path = "VisionComputacional/Examenes/Ex02/resources/train"
-videos = []
-trainVideos = os.listdir(vid_path) # List videos of the directory
-print("Total videos detected: ", len(trainVideos))
+# -------------------- IMAGES FOR TRAINING SVM ------------------------
+img_path = "VisionComputacional/Examenes/Ex02/resources/train/hog_img"
 
-# Loop to open and append videos
-for vid in trainVideos:
-    vidCur = cv2.VideoCapture(f'{vid_path}/{vid}')
-    if not vidCur.isOpened():
-        raise ValueError(f"Cannot open video from {vid_path}/{vid}")
-    videos.append(vidCur)
+hog_paths = os.listdir("VisionComputacional/Examenes/Ex02/resources/train/hog_img")
+print(hog_paths)
+hog_images = []
+hog_labels = ["calendar", "calendar", "calendar", "notebook", "notebook", "notebook", "raid", "raid", "raid"]
+
+hog_images = read_images(img_path, hog_paths)
+
+# Append all the test videos
+vid_path = "VisionComputacional/Examenes/Ex02/resources/test"
+videos = os.listdir(vid_path) # List videos of the directory
+print("Total videos detected: ", len(videos))
+
+# -----------------------------  ORB descriptor  -------------------------------
+orb_detector = ORB(images, classNames) # Create ORB descriptor
+#orb_detector.plot_comparison() # Show the keypoints of all the images
+
+# ----------------------------  SURF descriptor  -------------------------------
+sift_detector = SIFT(images, classNames) # Create SURF descriptor
+#sift_detector.plot_comparison() # Show the keypoints of all the images
+
+# ----------------------------- HOG descriptor ---------------------------------
+# Train model
+modelo = HOG_SVM(hog_images, hog_labels)
+modelo.train_SVM()
+
+# ----------------------------- OBJECT DETECTION -------------------------------
+metrics = []
+
+# Open video
+vid_path_full = "VisionComputacional/Examenes/Ex02/resources/test/video.mp4"
+vidCur = cv2.VideoCapture(vid_path_full)
+if not vidCur.isOpened():
+    raise ValueError(vid_path_full)
+
+# Get ORB metrics
+orb_metrics = orb_detector.compare_keypoints(vidCur)
+metrics.append(orb_metrics)
+
+# Reopen the video for SIFT
+vidCur.release()
+vidCur = cv2.VideoCapture(vid_path_full)
+
+# Get SIFT metrics
+sift_metrics = sift_detector.compare_keypoints(vidCur)
+metrics.append(sift_metrics)
+
+# Reopen the video for HOG + SVM
+vidCur.release()
+vidCur = cv2.VideoCapture(vid_path_full)
+
+# Get HOG + SVM metrics
+hog_metrics = modelo.classify_video(vidCur)
+metrics.append(hog_metrics)
+
+vidCur.release()
+
+print(pd.DataFrame(metrics))
